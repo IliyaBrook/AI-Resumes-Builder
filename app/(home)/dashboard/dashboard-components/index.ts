fd1952aa2d689c908c9e72c2a1b8410683980dce
Iliya Brook <iliyabrook1987@gmail.com>
2025-06-21 15:46:38 +0300

    change imports

--- app/(home)/dashboard/dashboard-components/index.ts
+++ app/(home)/dashboard/dashboard-components/index.ts
@@ -1,4 +1,4 @@
-import {default as AddResume} from './AddResume'
-import {default as ResumeItem} from './ResumeItem'
-import {default as ResumeList} from './ResumeList'
-import {default as TrashListBox} from './TrashListBox'
+export { default as AddResume } from "./AddResume";
+export { default as ResumeItem } from "./ResumeItem";
+export { default as ResumeList } from "./ResumeList";
+export { default as TrashListBox } from "./TrashListBox";

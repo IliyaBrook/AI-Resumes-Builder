67a42bf3171c62e8be60d7c5e2b1c580658a468a
brook <iliyabrook1987@gmail.com>
2025-09-11 17:31:11 +0300

    next-intl

--- i18n/request.ts
+++ i18n/request.ts
@@ -2,6 +2,7 @@ import { getRequestConfig } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import { routing } from './routing';
 
+// @ts-ignore
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
   if (!routing.locales.includes(locale as any)) notFound();
